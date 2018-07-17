1644a96d84bf19eb4c9206137d1d282c166cb6b5
rex <rex.chiu@outlook.com>
2018-07-17 12:44:36 -0400

    refactored and commented jquery counter code

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,6 +1,12 @@
 $(document).ready(function () {
-    $(".new-tweet").find("textarea").on("input", function() {
-        console.log(140 - $(this).val().length);
-        console.log($(this).closest("form").find(".counter").text(140 - $(this).val().length));
+    //grabs the jquery element of textarea in class new-tweet
+    var newTweetTextArea = $(".new-tweet").find("textarea");
+
+    //adds event listener for any changes in the input field
+    //calculates characters remaining and updates counter
+    newTweetTextArea.on("input", function() {
+        var charRemaining = 140 - $(this).val().length;
+        var counterElem = $(this).closest("form").find(".counter");
+        counterElem.text(charRemaining);
     });
 });
