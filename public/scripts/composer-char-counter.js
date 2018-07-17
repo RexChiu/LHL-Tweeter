f916b4160f1bff69a4fb912ac2ddb4d579746e22
rex <rex.chiu@outlook.com>
2018-07-17 12:40:31 -0400

    added code to modify the counter according to the value in the textarea

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
     $(".new-tweet").find("textarea").on("input", function() {
         console.log(140 - $(this).val().length);
+        console.log($(this).closest("form").find(".counter").text(140 - $(this).val().length));
     });
 });
