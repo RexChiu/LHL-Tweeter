6fe7f104635edcd07395c35971b2e22e37b6b12c
rex <rex.chiu@outlook.com>
2018-07-18 13:27:25 -0400

    added in client the ajax get request to fetch the tweets array from the server

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,57 +4,19 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
 */
 
-const tweetData = [
-    {
-        "user": {
-            "name": "Newton",
-            "avatars": {
-                "small": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-                "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-                "large": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-            },
-            "handle": "@SirIsaac"
-        },
-        "content": {
-            "text": "If I have seen further it is by standing on the shoulders of giants"
-        },
-        "created_at": 1461116232227
-    },
-    {
-        "user": {
-            "name": "Cats",
-            "avatars": {
-                "small": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-                "regular": "https://i.pinimg.com/originals/78/5c/39/785c39aa38a5867388fe432079f7808d.jpg",
-                "large": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-            },
-            "handle": "@Cats"
-        },
-        "content": {
-            "text": "Cats."
-        },
-        "created_at": 1461113959088
-    },
-    {
-        "user": {
-            "name": "Johann von Goethe",
-            "avatars": {
-                "small": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-                "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-                "large": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-            },
-            "handle": "@johann49"
-        },
-        "content": {
-            "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-        },
-        "created_at": 1461113796368
-    }
-];
-
 $(document).ready(function () {
+    let tweetData = null;
+
+    $.get("/tweets", function(resp, err){
+        if (err !== "success"){
+            console.log(err);
+        } else {
+            renderTweets(resp);
+        }
+    });
+
     //appends all the tweets onto the tweets container
-    renderTweets(tweetData);
+    //renderTweets(tweetData);
 
     $("#submit-new-tweet").on('submit',function (event) {
         event.preventDefault();
