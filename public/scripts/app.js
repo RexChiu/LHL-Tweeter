8b45fc70176adaed308794f861f11627b4ef03cd
rex <rex.chiu@outlook.com>
2018-07-20 17:51:50 -0400

    added some support for responsive design

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -51,7 +51,7 @@ $(document).ready(function() {
   });
 
   //event handler for the slide toggle for new tweet box
-  $("#nav-bar .compose-button").on("click", function(event) {
+  $(".nav-bar .compose-button").on("click", function(event) {
     $newTweet = $(".new-tweet");
     $newTweet.slideToggle();
     //focus on the new tweet if expanded
