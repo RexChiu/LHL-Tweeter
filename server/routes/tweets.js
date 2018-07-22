03608a79237458d5e29adde894f5d0bc3afcf774
RexChiu <rex.chiu@outlook.com>
2018-07-22 23:32:19 +0000

    added functionality to have a user post tweets

--- server/routes/tweets.js
+++ server/routes/tweets.js
@@ -31,31 +31,33 @@ module.exports = function(DataHelpers) {
         res.status(500).json({ error: err.message });
         res.error = true;
       } else {
-        //if user is found, redirect to index
-        if (user) {
-          res.redirect("../");
-          res.success = true;
+        //if user is not found, give error
+        if (!user) {
+          res
+            .status(400)
+            .json({ error: "Need to be logged in to compose tweet" });
+        } else {
+          const tweet = {
+            user: {
+              name: user.name,
+              avatar: user.avatar,
+              handle: user.handle
+            },
+            content: {
+              text: req.body.text
+            },
+            created_at: Date.now(),
+            likes: 0
+          };
+
+          DataHelpers.saveTweet(tweet, err => {
+            if (err) {
+              res.status(500).json({ error: err.message });
+            } else {
+              res.status(201).send();
+            }
+          });
         }
-      }
-    });
-
-    const user = req.body.user
-      ? req.body.user
-      : userHelper.generateRandomUser();
-    const tweet = {
-      user: user,
-      content: {
-        text: req.body.text
-      },
-      created_at: Date.now(),
-      likes: 0
-    };
-
-    DataHelpers.saveTweet(tweet, err => {
-      if (err) {
-        res.status(500).json({ error: err.message });
-      } else {
-        res.status(201).send();
       }
     });
   });
